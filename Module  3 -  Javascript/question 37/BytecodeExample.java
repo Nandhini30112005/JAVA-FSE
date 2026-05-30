4cf2d01dd3de1235df72fb4f1487c9f26e12a1e6
Nandhini30112005 <30nandhini.s@gmail.com>
2026-05-30 22:41:52 +0530

    Create BytecodeExample.java

---
+++ Module  3 -  Javascript/question 37/BytecodeExample.java
@@ -0,0 +1,10 @@
+public class BytecodeExample {
+
+    public void display() {
+        System.out.println("Bytecode Example");
+    }
+
+    public static void main(String[] args) {
+        new BytecodeExample().display();
+    }
+}
